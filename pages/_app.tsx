6c4063025cd8c7b46df6521bdb2f8a89363e9bd5
Ivan Luis Nuñez <ivanluis.cba@gmail.com>
2022-04-26 23:39:23 -0300

    Add metadata elements in html head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kronos</title>
+        <meta name="description" content="Daily schedule of UCC courses" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html className="dark">
+    <Html className="dark" lang="es">
       <Head />
       <body className="dark:bg-neutral-10 dark:text-white">
         <Main />
