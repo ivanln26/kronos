02e01ffe5ae7a2b46d08ad2f04c9dfe52431332c
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-03-31 23:15:52 -0300

    Update .env with prisma requirements

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    readonly MYSQL_HOST: string;
-    readonly MYSQL_PORT: string;
-    readonly MYSQL_USER: string;
     readonly MYSQL_PASSWORD: string;
-    readonly MYSQL_DBNAME: string;
+    readonly DATABASE_URL: string;
   }
 }
