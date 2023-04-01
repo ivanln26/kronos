476e1c9fab44f19ce89c985ea4efb1c79002912d
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-04-01 17:24:39 -0300

    Use trpc with superjson

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type AppRouter } from "@/server/routers/_app";
+import superjson from "superjson";
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
@@ -16,6 +17,7 @@ export const trpc = createTRPCNext<AppRouter>({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
+      transformer: superjson,
     };
   },
   /**
