aa1b2c0b7378934417f758191f13730dcb2cbfed
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-04-02 17:27:18 -0300

    Add google nextauth provider with env vars

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly MYSQL_PASSWORD: string;
     readonly DATABASE_URL: string;
+    readonly GOOGLE_CLIENT_ID: string;
+    readonly GOOGLE_CLIENT_SECRET: string;
   }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
-import type { AppProps } from "next/app";
+import { type AppType } from "next/app";
 import Head from "next/head";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { trpc } from "@/utils/trpc";
 
 import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App: AppType<{ session: Session | null }> = (
+  { Component, pageProps: { session, ...pageProps } },
+) => {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <title>Kronos</title>
         <meta name="description" content="Daily schedule of UCC courses" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
-}
+};
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(App);
