7a4e2f937ca0f1096b1ddfb1bf4a7289ec67c0de
Ivan Nunez <ivanluis.cba@gmail.com>
2023-04-07 03:11:06 -0300

    Validate and insert user to db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,33 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { type GoogleProfile } from "next-auth/providers/google";
+import { prisma } from "@/server/db";
+import { uccEmail, user } from "@/server/types";
 
 export const authOptions: NextAuthOptions = {
+  callbacks: {
+    async signIn({ profile }) {
+      return uccEmail.safeParse(profile?.email).success;
+    },
+    async jwt({ token, trigger, profile }) {
+      if (trigger == "signIn") {
+        const acc = profile as GoogleProfile;
+        const res = user.safeParse({
+          email: acc.email,
+          name: acc.given_name,
+          lastName: acc.family_name,
+          role: "student",
+        });
+        if (res.success) {
+          await prisma.user.upsert({
+            where: { ucc: res.data.ucc },
+            update: {},
+            create: res.data,
+          });
+        }
+      }
+      return token;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
