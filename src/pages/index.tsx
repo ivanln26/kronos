5edc65d92179833baca0331edd0de6c7e5de34a5
Ivan Nunez <ivanluis.cba@gmail.com>
2023-04-26 22:27:50 -0300

    Refactor week navbar state management

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,52 +8,32 @@ import Week from "@/components/Week";
 import { trpc } from "@/utils/trpc";
 import { Weekday } from "@/server/types";
 
+type Day = {
+  id: Weekday;
+  name: string;
+};
+
 const Home: NextPage = () => {
-  const initialDays = [
-    { name: "L", active: false },
-    { name: "M", active: false },
-    { name: "X", active: false },
-    { name: "J", active: false },
-    { name: "V", active: false },
+  const days: Day[] = [
+    { id: "monday", name: "L" },
+    { id: "tuesday", name: "M" },
+    { id: "wednesday", name: "X" },
+    { id: "thursday", name: "J" },
+    { id: "friday", name: "V" },
   ];
 
-  const mapWeekday = (i: number): Weekday => {
-    switch (i) {
-      case 0:
-        return "monday";
-      case 1:
-        return "tuesday";
-      case 2:
-        return "wednesday";
-      case 3:
-        return "thursday";
-      default:
-        return "friday";
-    }
-  };
-
   const [currentDay, setCurrentDay] = useState<Weekday>("monday");
-  const [days, setDays] = useState(initialDays);
 
   const lectures = trpc.lecture.getByDay.useQuery({ day: currentDay });
 
-  useEffect(() => {
-    let days = [...initialDays];
-    days[0].active = true;
-    setDays(days);
-  }, []);
-
-  const updateDays = (i: number) => {
-    let days = [...initialDays];
-    days[i].active = true;
-    setDays(days);
-    setCurrentDay(mapWeekday(i));
+  const updateCurrent = (weekday: Weekday) => {
+    setCurrentDay(weekday);
   };
 
   return (
     <>
       <Navbar />
-      <Week days={days} setDays={updateDays} />
+      <Week days={days} activeDay={currentDay} setDays={updateCurrent} />
       <div className="flex justify-center px-2">
         <div className="flex flex-col basis-full gap-y-2">
           {lectures.data &&
