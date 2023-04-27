23b7e9911641523751a90cc5e2e128b792642e19
Ivan Nunez <ivanluis.cba@gmail.com>
2023-04-26 22:55:21 -0300

    Scale week navbar

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,4 +1,5 @@
 import { Weekday } from "@prisma/client";
+import { Roboto_Mono } from "next/font/google";
 
 import Button from "./Button";
 
@@ -13,14 +14,20 @@ type WeekProps = {
   setDays: (i: Weekday) => void;
 };
 
+const robotoMono = Roboto_Mono({
+  weight: "700",
+  subsets: ["latin"],
+});
+
 const Week = ({ days, activeDay, setDays }: WeekProps) => {
   return (
-    <div className="p-2">
+    <div className={`p-2 ${robotoMono.className}`}>
       <div className="flex justify-between p-2 rounded-md font-bold select-none bg-primary-99 bg-gradient-to-r from-primary-40/[.05] to-primary-40/[.05] dark:bg-neutral-10 dark:from-primary-80/[.05] dark:to-primary-80/[.05]">
         {days.map(({ id, name }) => (
           <Button
             key={id}
             aspect={activeDay == id ? "contained" : "text"}
+            size="large"
             onClick={() => setDays(id)}
             value={id}
           >
