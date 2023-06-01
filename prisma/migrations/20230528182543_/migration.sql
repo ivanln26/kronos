b689f0ad9351c465b6c17347ceb577f2928fa665
Ivan Nunez <ivanluis.cba@gmail.com>
2023-06-01 19:13:05 -0300

    Remove prisma migration files

--- prisma/migrations/20230528182543_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [cancelled] on the enum `lecture_state` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `lecture` MODIFY `state` ENUM('scheduled', 'ongoing', 'canceled', 'delayed') NOT NULL;
